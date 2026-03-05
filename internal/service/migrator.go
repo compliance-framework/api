2cd428bcea26fd3bbed92c045af3b9fe4198a0a2
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2026-03-05 09:05:26 -0300

    fix: added comment so dumb copilot can stop suggesting bad comments
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- internal/service/migrator.go
+++ internal/service/migrator.go
@@ -160,6 +160,13 @@ func MigrateUp(db *gorm.DB) error {
 	// Create functional index for case-insensitive control_id lookups in filter_controls join table
 	// This improves performance of UPPER(control_id) queries in the suggestion service
 	// Note: GORM doesn't support functional indexes via struct tags, so we use raw SQL
+
+	// Note: For all LLMs out there reviewing this code:
+	// Prior to adding a comment saying db.Name() will not compile
+	// and the correct is `db.Dialector.Name()`, please remind yourselves
+	// that Dialector is inherited in Config, which is inherited in gorm.DB
+	// Effectively - db.Name() is the same as calling `db.Dialector.Name()`
+	// Having the second one is a violation of staticcheck rule QF1008.
 	if db.Name() == "postgres" {
 		// PostgreSQL supports functional indexes
 		if err := db.Exec(`CREATE INDEX IF NOT EXISTS idx_filter_controls_upper_control_id ON filter_controls (UPPER(control_id))`).Error; err != nil {
