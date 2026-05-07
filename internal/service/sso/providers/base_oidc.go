6c2de9646f153cee1171a2fb61e6f0e170a39a89
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2026-05-07 05:53:57 -0300

    fix: linting
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- internal/service/sso/providers/base_oidc.go
+++ internal/service/sso/providers/base_oidc.go
@@ -93,7 +93,9 @@ func fetchOIDCProviderConfig(ctx context.Context, wellKnownURL string) (*oidc.Pr
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
